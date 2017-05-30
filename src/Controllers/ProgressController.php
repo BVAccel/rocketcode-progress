d31dae825ca30308842bc4a6141dfb825f093ef4
Tony <tony.santucci@gmail.com>
2017-05-30 18:51:14 +0000

    Apply fixes from StyleCI

--- src/Controllers/ProgressController.php
+++ src/Controllers/ProgressController.php
@@ -2,11 +2,11 @@
 
 namespace Rocketcode\Progress\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 use Rocketcode\Progress\Progress;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ProgressController extends BaseController
 {
